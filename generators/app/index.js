712f163c6f89ee0fe82b977745e12c3b3ceeb6c6
Rudy Baer <rudy@bearstudio.fr>
2019-02-27 13:15:35 +0100

    fix dedicated build
    add missing catch to manage response error from api

--- generators/app/index.js
+++ generators/app/index.js
@@ -310,7 +310,7 @@ module.exports = class extends BaseGenerator {
         var applicationId = getCurrentApplicationId(this.props.alias);
         return client.owner(this.props.user).applications._.put()
             .withParams( [this.props.user, applicationId])
-            .send(JSON.stringify({separateBuild:true})).toPromise().toPromise().catch(error => {
+            .send(JSON.stringify({separateBuild:true})).toPromise().catch(error => {
                 this.log(`${chalk.yellow.bold('WARNING!')} Unable to dedicated build instance.`);
             });
     }
